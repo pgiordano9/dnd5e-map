bbe92ae97aed976ad388ad2e726d4679b5904f77
Peter Giordano <peter.giordano@fisglobal.com>
2024-02-19 21:14:14 +0000

    start drawing lines with html canvas element.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,27 @@
 import * as React from "react"
+import { useEffect } from "react"
 
 const pageStyles = {
   color: "#232129",
   padding: 96,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
 }
+
 const IndexPage = () => {
+  useEffect(() => { 
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    var x = 0;
+    var y = 0;
+
+    ctx.moveTo(0, 0);
+    ctx.lineTo(0, 100);
+    ctx.stroke();
+  });
+
   return (
     <main style={pageStyles}>
+      <canvas id="myCanvas" width="200" height="100"></canvas>
     </main>
   )
 }
