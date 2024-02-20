5b531bc2e26fb27311c2e55712ef2d656a72f985
Peter Giordano <peter.giordano@fisglobal.com>
2024-02-20 11:55:12 +0000

    movement with presses including diagonal movement.

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,38 +24,92 @@ const IndexPage = () => {
       }
     }
 
-    ctx.beginPath();
-    ctx.arc(60, 60, 20, 0, 2 * Math.PI);
-    ctx.stroke();
-    ctx.fillStyle = "red";
-    ctx.fill();
-    
-    document.addEventListener("keydown", logKey);
+    drawGrid();
 
-    let x = canvas.width / 2;
-    let y = canvas.height - 30;
-    let dx = 2;
-    let dy = -2;
-    
-    function draw() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      drawGrid();
+    let redX = 60;
+    let redY = 60;
+
+    function drawCircle() {
       ctx.beginPath();
-      ctx.arc(x, y, 10, 0, Math.PI * 2);
-      ctx.fillStyle = "#0095DD";
+      ctx.arc(redX, redY, 20, 0, 2 * Math.PI);
+      ctx.stroke();
+      ctx.fillStyle = "blue";
       ctx.fill();
-      ctx.closePath();
-      x += dx;
-      y += dy;
     }
 
-    setInterval(draw, 1);
+    drawCircle();
+    
+    document.addEventListener("keydown", listen);
+
+//    let x = canvas.width / 2;
+//    let y = canvas.height - 30;
+//    let dx = 2;
+//    let dy = -2;
+//    
+//    function draw() {
+//      ctx.clearRect(0, 0, canvas.width, canvas.height);
+//      drawGrid();
+//      ctx.beginPath();
+//      ctx.arc(x, y, 10, 0, Math.PI * 2);
+//      ctx.fillStyle = "#0095DD";
+//      ctx.fill();
+//      ctx.closePath();
+//      x += dx;
+//      y += dy;
+//    }
+
+    //setInterval(draw, 1);
+    function listen(e) {
+      console.log(e.code);
+      if (e.code === "KeyS") {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        redY += 40;
+        drawCircle();
+        drawGrid();
+      } else if (e.code === "KeyD") {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        redX += 40;
+        drawCircle();
+        drawGrid();
+      } else if (e.code === "KeyW") {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        redY -= 40;
+        drawCircle();
+        drawGrid();
+      } else if (e.code === "KeyA") {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        redX -= 40;
+        drawCircle();
+        drawGrid();
+      } else if (e.code === "KeyQ") {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        redX -= 40;
+        redY -= 40;
+        drawCircle();
+        drawGrid();
+      } else if (e.code === "KeyE") {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        redX += 40;
+        redY -= 40;
+        drawCircle();
+        drawGrid();
+      } else if (e.code === "Backquote") {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        redX -= 40;
+        redY += 40;
+        drawCircle();
+        drawGrid();
+      } else if (e.code === "KeyC") {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        redX += 40;
+        redY += 40;
+        drawCircle();
+        drawGrid();
+      }
+    }
 
     });
 
-  function logKey(e) {
-    console.log(` ${e.code}`);
-  }
 
 
   return (
